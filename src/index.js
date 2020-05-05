200c58fae007f7a95baf8834b04d17cd0b13c2eb
kigama <kavanegasm>
2020-05-04 19:52:18 -0500

    QA apollo cli in Prueba.js

--- src/index.js
+++ src/index.js
@@ -1,55 +1,22 @@
-import React from "react";
-import App from "./App";
-import { render } from "react-dom";
-import ApolloClient , { gql } from "apollo-boost";
-import { ApolloProvider,  useQuery} from "@apollo/react-hooks";
+import React from 'react';
+import { render } from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hoc';
+
+
+import App from './App';
+import Prueba from './Prueba';
 
 
 const client = new ApolloClient({
   uri: "http://104.198.185.230:3000/"
 });
 
-function CharactersQuery() {
-  const { loading, error, data } = useQuery(gql`
-  {
-    publications
-    {
-      _id
-      title
-      description
-      state_publication
-      contact_information
-      id_image
-      stock
-      expiration_date
-      price
-      categories
-    }
-  }
-  `);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  return data.publications.map(({_id, title, description}) => (
-    <div key={_id}>
-      <p>
-        <li>{_id}: {title} {description}</li>
-      </p>
-    </div>
-  ));
-}
-
-
-
-
-const Appis = () => (
+const WrappedApp = (
   <ApolloProvider client={client}>
-    <div>
-    <CharactersQuery />
-      <App/>
-    </div>
+    <Prueba/>
+    <App />
   </ApolloProvider>
 );
 
-render(<Appis />, document.getElementById("root"));
+render(WrappedApp, document.getElementById('root'));
