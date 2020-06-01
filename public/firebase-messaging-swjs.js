e8f42c95648df0a442e4ad9191a187d19be847d1
kavanegasm <kavanegasm@edu.co.com>
2020-05-31 19:40:09 -0500

    Revert "add firebase account free billing off"
    This reverts commit 81cab8c084731752150278d3227e9cea90de1f34.

--- public/firebase-messaging-swjs.js
+++
@@ -1,8 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js')
-
-firebase.initializeApp{
-    messagingSenderId: "507383070115"
-}
-
-const initMessaging = firebase.messaging()
